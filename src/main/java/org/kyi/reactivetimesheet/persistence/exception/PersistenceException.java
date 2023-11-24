39780eeba7b47b9b218428f52d5426d24aff8b3f
heinhtetaungms <hha.heinms@gmail.com>
2023-11-24 11:36:06 +0630

    add: exception

---
+++ src/main/java/org/kyi/reactivetimesheet/persistence/exception/PersistenceException.java
@@ -0,0 +1,7 @@
+package org.kyi.reactivetimesheet.persistence.exception;
+
+public class PersistenceException extends RuntimeException{
+    public PersistenceException(String message) {
+        super(message);
+    }
+}
