c603d3af22c7ee359f2fe1cf40ecad575424459e
heinhtetaungms <hha.heinms@gmail.com>
2023-11-24 10:34:16 +0630

    Initial commit

---
+++ src/main/java/org/kyi/reactivetimesheet/ReactiveTimesheetApplication.java
@@ -0,0 +1,13 @@
+package org.kyi.reactivetimesheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveTimesheetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveTimesheetApplication.class, args);
+    }
+
+}
