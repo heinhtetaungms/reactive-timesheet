0ffb9c425ef64131b8d83407e330c12017c56c81
heinhtetaungms <hha.heinms@gmail.com>
2023-11-24 14:57:37 +0630

    add: exception class

---
+++ src/main/java/org/kyi/reactivetimesheet/persistence/exception/TeamServiceException.java
@@ -0,0 +1,7 @@
+package org.kyi.reactivetimesheet.persistence.exception;
+
+public class TeamServiceException extends RuntimeException{
+    public TeamServiceException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/kyi/reactivetimesheet/persistence/exception/TimeEntryServiceException.java
@@ -0,0 +1,7 @@
+package org.kyi.reactivetimesheet.persistence.exception;
+
+public class TimeEntryServiceException extends RuntimeException{
+    public TimeEntryServiceException(String message) {
+        super(message);
+    }
+}
